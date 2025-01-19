e91635a4b5537b4a2a56cd16dbc7fb9bfb0fc4b4
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2025-01-19 19:04:34 +0530

    add routing and game page; integrate react-router-dom for navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
 import MainPage from './containers/MainPage';
+import { BrowserRouter, Routes, Route } from "react-router";
+import GamePage from './containers/GamePage';
 
 function App() {
   return (
-    <div className="App-Prison">
-      <MainPage />
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/game" element={<GamePage />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 function MainPage() {
   return (
-    <div>
+    <div style={{textAlign: 'center'}}>
         <h1>WELCOME TO PRISON SURVIVAL SIM</h1>
+        <a href='/game'><button>Go to game page</button></a> 
     </div>
   )
 }
