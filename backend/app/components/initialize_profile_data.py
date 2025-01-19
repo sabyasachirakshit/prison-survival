9fbc56bf0b434f4aed401995d400e42610e261b7
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2025-01-19 21:13:33 +0530

    trying

---
+++ backend/app/components/initialize_profile_data.py
@@ -0,0 +1,5 @@
+import random
+
+crimes=["Theft","Assault","Vandalism","Fraud","Burglary","Robbery","Arson","Kidnapping","Homicide"]
+def select_crime():
+    return random.choice(crimes)
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,12 @@
 from flask import jsonify, request
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import json
+from components.initialize_profile_data import select_crime
+
+
+
 
 PROFILE_LIST_FILE = "./profiles_list.json"
 PROFILE_DATA_FILE = "./profiles_data.json"
@@ -16,9 +23,13 @@ def read_profiles(check=False):
         print("File not found. Returning an empty list.") 
         return []
 
-def write_profiles(data):
-    with open(PROFILE_LIST_FILE, "w") as file:
-        json.dump(data, file, indent=4)
+def write_profiles(data,check=False):
+    if check:
+        with open(PROFILE_DATA_FILE, "w") as file:
+            json.dump(data, file, indent=4)
+    else:
+        with open(PROFILE_LIST_FILE, "w") as file:
+            json.dump(data, file, indent=4)
 
 # Function to register routes
 def register_routes(app):
@@ -40,7 +51,13 @@ def register_routes(app):
     @app.route('/api/profiles', methods=['POST'])
     def add_profile():
         new_profile = request.json
+        # Append the crime data to the new profile
+        new_profile['crime'] = select_crime()
         profiles = read_profiles(False)
         profiles.append(new_profile)
-        write_profiles(profiles)
+        write_profiles(profiles,False)
+        
+        profiles_data = read_profiles(True)
+        profiles_data.append(new_profile)
+        write_profiles(profiles_data, is_data=True)
         return jsonify(new_profile), 201
