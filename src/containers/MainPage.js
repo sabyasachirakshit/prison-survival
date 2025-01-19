7c6b508b7e800a1a331f693d50eef232aea5a19b
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2025-01-19 19:18:30 +0530

    add react-device-detect for responsive profile slots; style MainPage with CSS

--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import '../styles/MainPage.css'
+import { isMobile } from 'react-device-detect';
 
 function MainPage() {
   return (
     <div style={{textAlign: 'center'}}>
         <h1>WELCOME TO PRISON SURVIVAL SIM</h1>
-        <a href='/game'><button>Go to game page</button></a> 
+        <div className="all-profile-slots" style={!isMobile?{display:"flex",justifyContent:"center",width:"100%"}:{flexDirection:"column",gap:10,display:"flex",alignItems:"center"}}>
+            <div className="profile-slot" style={isMobile?{width:"100px"}:{}}>+ Add Profile</div>
+            <div className="profile-slot" style={isMobile?{width:"100px"}:{}}>+ Add Profile</div>
+            <div className="profile-slot" style={isMobile?{width:"100px"}:{}}>+ Add Profile</div>
+            <div className="profile-slot" style={isMobile?{width:"100px"}:{}}>+ Add Profile</div>
+            <div className="profile-slot" style={isMobile?{width:"100px"}:{}}>+ Add Profile</div>
+            <div className="profile-slot" style={isMobile?{width:"100px"}:{}}>+ Add Profile</div>
+            <div className="profile-slot" style={isMobile?{width:"100px"}:{}}>+ Add Profile</div>
+            <div className="profile-slot" style={isMobile?{width:"100px"}:{}}>+ Add Profile</div>
+            <div className="profile-slot" style={isMobile?{width:"100px"}:{}}>+ Add Profile</div>
+            <div className="profile-slot" style={isMobile?{width:"100px"}:{}}>+ Add Profile</div>
+        </div>
     </div>
   )
 }
